8c865b45b80e4b4739e8905b69018295f0de719f
SamOxzu <samgutierrezsa@unal.edu.co>
2023-08-22 09:52:17 -0500

    Modificación Clase Auto
    Se modificó el funcionamiento del atributo "Asientos" en la clase Auto

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 class Auto:
     cantidadCreados = 0
-    def __init__(self, modelo, precio, marca, motor, registro):
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
-        self.asientos = []
+        self.asientos = asientos
         self.marca = marca
         self.motor = motor
         self.registro = registro
