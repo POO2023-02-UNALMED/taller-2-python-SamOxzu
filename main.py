7c72e9dd4c72be3ada2bb55a3996b17da2c0c54b
SamOxzu <samgutierrezsa@unal.edu.co>
2023-08-22 09:47:10 -0500

    Reinicio debido a fallos
    Se realizó el código nuevamente desde 0.

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-class Motor:
-    def __init__(self, numeroCilindros, tipo, registro):
-        self.numeroCilindros = numeroCilindros
-        self.tipo = tipo
-        self.registro = registro
-
-    def cambiarRegistro(self, registro):
-        self.registro = registro
-
-    def asignarTipo(self, tipo):
-        if tipo == "electrico" or tipo == "gasolina":
-            self.tipo = tipo
-
-
 class Auto:
+    cantidadCreados = 0
     def __init__(self, modelo, precio, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
@@ -20,15 +7,14 @@ class Auto:
         self.marca = marca
         self.motor = motor
         self.registro = registro
-        self.cantidadCreados = 0
-
+    
     def cantidadAsientos(self):
         numAsientos = 0
         for i in range(len(self.asientos)):
             if self.asientos[i] != None:
                 numAsientos += 1
         return numAsientos
-
+    
     def verificarIntegridad(self):
         if self.registro == self.motor.registro:
             for i in range(len(self.asientos)):
@@ -39,14 +25,25 @@ class Auto:
         else:
             return "Las piezas no son originales"
 
-
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    
+    def asignarTipo(self, tipo):
+        if tipo == "electrico" or tipo == "gasolina":
+            self.tipo = tipo
 
 class Asiento:
     def __init__(self, color, precio, registro):
         self.color = color
         self.precio = precio
         self.registro = registro
-
+    
     def cambiarColor(self, color):
         if color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
             self.color = color
